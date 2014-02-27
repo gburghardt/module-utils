34d1eae0b0c972fb00274c1b30ed869b87f80497
Greg Burghardt <greg_burghardt@yahoo.com>
2014-02-27 15:41:56 -0500

    Added Module.Utils.Rendering

---
+++ src/module/utils/rendering.js
@@ -0,0 +1,23 @@
+Module.Utils.Rendering = {
+
+	included: function(Klass) {
+		Klass.addCallback("destroy", "_destroyRenderingEngine");
+	}
+
+	prototype: {
+
+		renderingEngine: null,
+
+		_destroyRenderingEngine: function _destroyRenderingEngine() {
+			this.renderingEngine = null;
+		},
+
+		render: function render(name, data, elementOrId) {
+			return this.renderingEngine.render(name, data, elementOrId);
+		}
+
+	}
+
+};
+
+Module.Utils.include(Module.Utils.Rendering);
