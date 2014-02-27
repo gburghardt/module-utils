eb9e9115edab1451187ad89c5959aded4728af11
Greg Burghardt <greg_burghardt@yahoo.com>
2014-02-27 15:24:26 -0500

    Added Module.Utils.ElementStore. Removed Module.Utils.Events

---
+++ src/module/utils/element_store.js
@@ -0,0 +1,16 @@
+Module.Utils.ElementStore = {
+
+	included: function(Klass) {
+		Klass.addCallback("beforeReady", "_initElementStore");
+		Klass.addCallback("destroy", "destroyElementStore");
+	},
+
+	prototype: {
+		_initElementStore: function _initElementStore() {
+			this.initElementStore(this.element);
+		}
+	}
+
+};
+
+Module.Utils.include(Module.Utils.ElementStore);
